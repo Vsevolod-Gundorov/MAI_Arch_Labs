49a9ae2ab21e87493e57b372da0b716fabc4f702
Vsevolod-Gundorov <70602017+Vsevolod-Gundorov@users.noreply.github.com>
2024-06-05 08:34:53 +0300

    Update user_handler_factory.cpp

--- lab2/user_server/user_handler_factory.cpp
+++ lab2/user_server/user_handler_factory.cpp
@@ -1,5 +1,5 @@
 #include "user_handler_factory.h"
-#include "../user_request_handler/user_handler.h"
+#include "../user_handler.h"
 
 Poco::Net::HTTPRequestHandler* UserHandlerFactory::createRequestHandler(const Poco::Net::HTTPServerRequest & request){
     return new MessageHandler;
